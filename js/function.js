36bc8f710dbd13be517d43aaabd37ce735d7ef67
Juliana Herrera <juliana.manco038@pascualbravo.edu.co>
2024-10-18 15:42:54 -0500

    Añadiendo funciones a mi proyecto

---
+++ js/function.js
@@ -0,0 +1,13 @@
+
+
+
+function calcularArea () {
+    base = document.getElementById('base').value;
+    altura = document.getElementById('altura').value;
+    area = 0;
+
+    area = (base * altura)/2
+
+    alert(area)
+}
+
